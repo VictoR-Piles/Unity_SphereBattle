33e4a3870cdc79fce3b29f56c11e9e5b82b1809c
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-06-04 14:07:23 +0200

    Sistema de PowerUps terminados

--- SphereBattle/Assets/Scripts/PlayerController.cs
+++ SphereBattle/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
+	public float speed = 10f;
+	public float powerUpImpulseStrenght = 15f;
 
-    private Rigidbody rb;
-    private GameObject centerP;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        centerP = GameObject.Find("CenterPoint");
-    }
+	private Rigidbody rb;
+	private GameObject centerP;
+	private bool hasPowerUp = false;
 
-    void Update()
-    {
-        float forwardInput = Input.GetAxis("Vertical");
-        rb.AddForce(centerP.transform.forward * (speed * forwardInput));
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		centerP = GameObject.Find("CenterPoint");
+	}
+
+	void Update()
+	{
+		float forwardInput = Input.GetAxis("Vertical");
+		rb.AddForce(centerP.transform.forward * (speed * forwardInput));
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("PowerUp"))
+		{
+			hasPowerUp = true;
+			Destroy(other.gameObject);
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Enemy") && hasPowerUp)
+		{
+			Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+			Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+			
+			enemyRb.AddForce(awayFromPlayer * powerUpImpulseStrenght, ForceMode.Impulse);
+			Debug.Log($"Collided with {other.gameObject.name} with hasPowerUp set to {hasPowerUp}");
+		}
+	}
 }
