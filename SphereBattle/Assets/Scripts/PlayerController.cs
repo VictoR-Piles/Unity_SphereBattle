55dcbe96181e27a88bbadb4d51e7d527307acf7f
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-06-04 13:13:27 +0200

    Mecanicas basicas terminadas

---
+++ SphereBattle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody rb;
+    private GameObject centerP;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        centerP = GameObject.Find("CenterPoint");
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(centerP.transform.forward * (speed * forwardInput));
+    }
+}
---
+++ SphereBattle/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+	public float rotationSpeed = 50f;
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		transform.Rotate(Vector3.up, (rotationSpeed * horizontalInput * Time.deltaTime));
+	}
+}
